e7d030218e653e65250ac5f2c2b12f06b4d0f397
juanEurquijo <juancho.urquijo@gmail.com>
2023-11-07 21:05:34 -0500

    first commit

---
+++ apigateway/src/main/java/co/edu/javeriana/arquitecture/apigateway/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package co.edu.javeriana.arquitecture.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+        //configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ discovery/src/test/java/co/edu/javeriana/arquitecture/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.arquitecture.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
